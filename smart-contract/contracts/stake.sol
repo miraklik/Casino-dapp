79bf72c05c77b80f1e02461cf90293e5f8d97fa8
mirak <karmau473@gmail.com>
2025-04-02 18:29:46 +0300

    add frontend

--- smart-contract/contracts/stake.sol
+++ smart-contract/contracts/stake.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.29;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Stake is Ownable {
+contract Stake is Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
+
     IERC20 private immutable stakingToken;
     IERC20 private immutable rewardToken;
 
@@ -12,60 +16,74 @@ contract Stake is Ownable {
     uint256 public finishAt;
     uint256 public updatedAt;
     uint256 public rewardRate;
-    uint256 public rewartPerTokenStored;
+    uint256 public rewardPerTokenStored;
     uint256 public totalSupply;
 
+    // custom error
     error InvalidAmount();
     error InvalidRewardRate();
     error InvalidRewardAmount();
     error RewardDurationNotFinished();
 
+    // mappings
     mapping(address => uint256) public userRewardsPerTokenPaid;
     mapping(address => uint256) public rewards;
     mapping(address => uint256) public balanceOf;
 
+    // Events
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    /*
+     * @notice Initializes the Stake contract with Chainlink VRF and VIP NFT contract.
+     * @param _stakingToken Address of the staking token contract.
+     * @param _rewardToken Address of the reward token contract.
+    */
     constructor(address _stakingToken, address _rewardToken) Ownable(msg.sender){
         stakingToken = IERC20(_stakingToken);
         rewardToken = IERC20(_rewardToken);
     }
 
     modifier updateReward(address _account) {
-        rewartPerTokenStored = rewardPerToken();
-        updatedAt = lastTimeRewardApplicade();
+        rewardPerTokenStored = rewardPerToken();
+        updatedAt = lastTimeRewardApplicable();
 
         if (_account != address(0)) {
             rewards[_account] = earned(_account);
-            userRewardsPerTokenPaid[_account] = rewartPerTokenStored;
+            userRewardsPerTokenPaid[_account] = rewardPerTokenStored;
         }
         _;
     }
 
-    function lastTimeRewardApplicade() public view returns (uint256) {
+    function lastTimeRewardApplicable() public view returns (uint256) {
         return _min(finishAt, block.timestamp);
     }
 
     function rewardPerToken() public view returns (uint256) {
         if (totalSupply == 0) {
-            return rewartPerTokenStored;
+            return rewardPerTokenStored;
         }
 
-        return rewartPerTokenStored
-            + (rewardRate * (lastTimeRewardApplicade() - updatedAt) * 1e18)
+        return rewardPerTokenStored
+            + (rewardRate * (lastTimeRewardApplicable() - updatedAt) * 1e18)
                 / totalSupply;
     }
 
-    function stake(uint256 _amount) external updateReward(msg.sender) {
+    function stake(uint256 _amount) external updateReward(msg.sender) nonReentrant {
         require(_amount > 0, InvalidAmount());
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
+        stakingToken.safeTransferFrom(msg.sender, address(this), _amount);
         balanceOf[msg.sender] += _amount;
         totalSupply += _amount;
+        emit Staked(msg.sender, _amount);
     }
 
-    function withdraw(uint256 _amount) external updateReward(msg.sender) {
+    function withdraw(uint256 _amount) external updateReward(msg.sender) nonReentrant {
         require(_amount > 0, InvalidAmount());
-        balanceOf[msg.sender] += _amount;
+        balanceOf[msg.sender] -= _amount;
         totalSupply -= _amount;
-        stakingToken.transfer(msg.sender, _amount);
+        stakingToken.safeTransfer(msg.sender, _amount);
+        emit Withdrawn(msg.sender, _amount);
     }
 
     function earned(address _account) public view returns (uint256) {
@@ -77,12 +95,14 @@ contract Stake is Ownable {
         ) + rewards[_account];
     }
 
-    function getReward() external updateReward(msg.sender) {
+    function getReward() external updateReward(msg.sender) nonReentrant{
         uint256 reward = rewards[msg.sender];
         if (reward > 0) {
             rewards[msg.sender] = 0;
             rewardToken.transfer(msg.sender, reward);
         }
+
+        emit RewardPaid(msg.sender, reward);
     }
 
     function setRewardsDuration(uint256 _duration) external onlyOwner {
