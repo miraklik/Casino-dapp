96878d607a5256f4d1fc0beff19a81247b384d0f
mirak <karmau473@gmail.com>
2025-04-03 16:32:31 +0300

    add votes system

--- smart-contract/contracts/Token.sol
+++ smart-contract/contracts/Token.sol
@@ -1,17 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Permit, Nonces} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20, ERC20Burnable, ERC20Permit, Ownable {
+contract Token is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes, Ownable {
     constructor() ERC20("LUCKY", "LCK") ERC20Permit("LUCKY") Ownable(msg.sender) {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
     function mint(address _to, uint256 amount) external onlyOwner{
         _mint(_to, amount);
+    }
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
     }
 }
